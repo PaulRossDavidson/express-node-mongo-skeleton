1bf15b6c2176a8c85a2a0c2be6738d2a8b8144d8
PaulRossDavidson <paulrossdavidson@gmail.com>
2015-09-24 14:34:57 -0700

    removed edit/delete buttons from stie.

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,35 @@ var myApp = angular.module("TacoApp", ['ngMaterial','ngMdIcons']);
 myApp.run(['$rootScope',function($rootScope){
 	console.log("rootScope added.")
 }])
-.controller('AppCtrl', ['$scope', function($scope) {
+.controller('AppCtrl', ['$scope', $mdDialog, function($scope, $mdDialog) {
 	console.log('controller loaded')
 	$scope.blob = {name: "Paul"}
 
 
 }]);
+// $scope.showAdvanced = function(ev) {
+//     $mdDialog.show({
+//       controller: DialogController,
+//       templateUrl: 'dialog1.tmpl.html',
+//       parent: angular.element(document.body),
+//       targetEvent: ev,
+//       clickOutsideToClose:true
+//     })
+//     .then(function(answer) {
+//       $scope.status = 'You said the information was "' + answer + '".';
+//     }, function() {
+//       $scope.status = 'You cancelled the dialog.';
+//     });
+//   };
+// });
+// function DialogController($scope, $mdDialog) {
+//   $scope.hide = function() {
+//     $mdDialog.hide();
+//   };
+//   $scope.cancel = function() {
+//     $mdDialog.cancel();
+//   };
+//   $scope.answer = function(answer) {
+//     $mdDialog.hide(answer);
+//   };
+// }
